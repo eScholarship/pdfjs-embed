2a8f7419cbd74164f020b385e5790f1373e59f38
Martin Haye <martin.haye@ucop.edu>
2016-12-20 15:58:14 -0800

    Avoid duplicating worker code in main bundle.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 require("./js/compatibility.js");
 require("./js/l10n.js");
 require("./js/pdf.js");
-window.PDFJS.workerSrc = require("./js/pdf.worker.js");
 // Weird stuff that just works. This serves to demonstrate that:
 // (A) Martin doesn't understand PDFJS, and 
 // (B) Martin doesn't understand node.require()
